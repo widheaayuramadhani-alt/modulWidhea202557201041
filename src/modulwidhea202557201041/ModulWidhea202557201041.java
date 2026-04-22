177a736099669e20451a6aaec6b83c5a223e4221
dhea <widheaayuramadhani@gmail.com>
2026-04-22 14:34:18 +0700

    commit keempat array

--- src/modulwidhea202557201041/ModulWidhea202557201041.java
+++ src/modulwidhea202557201041/ModulWidhea202557201041.java
@@ -15,7 +15,6 @@ public class ModulWidhea202557201041 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
          // TODO code application logic here
         int data[][] = {{4,6,4,2,8,4,2,10}, {4,6,4,2,8,4,2,10}};
             
