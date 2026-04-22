93dfd19f4ebc5e4684b7faa768a24d82731c364d
dhea <widheaayuramadhani@gmail.com>
2026-04-22 13:57:09 +0700

    commit ketiga array

--- src/modulwidhea202557201041/ModulWidhea202557201041.java
+++ src/modulwidhea202557201041/ModulWidhea202557201041.java
@@ -8,6 +8,7 @@ package modulwidhea202557201041;
  *
  * @author asus4
  */
+import javax.swing.JOptionPane;
 public class ModulWidhea202557201041 {
 
     /**
@@ -15,6 +16,85 @@ public class ModulWidhea202557201041 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         // TODO code application logic here
+        int data[][] = {{4,6,4,2,8,4,2,10}, {4,6,4,2,8,4,2,10}};
+            
+        int total = 0;
+        int jumlahdata = 0;
+        for (int a = 0; a < data.length; a++) {
+            for (int b = 0; b < data[a].length; b++) {
+            total += data[a][b]; jumlahdata++;
+            System.out.print(data[a][b] + "");
+            }
+            System.out.print("\n");
+        }
+        //Menghitung Rata-Rata
+            double ratarata = (double) total / jumlahdata;
+            
+            System.out.println("Total:" + total);
+            System.out.println("JumlahData:" + jumlahdata);
+            System.out.println("RataRata:" + ratarata);
+            
+            
+            //JoptionPane mencari dan mengganti nilai
+            int cari = Integer.parseInt(JOptionPane.showInputDialog("Masukkan angka yang dicari:"));
+            int nilaibaru=0;
+            boolean ditemukan = false;
+            String alamat="";
+            
+            for (int a = 0; a < data.length; a++) {
+                for (int b = 0; b < data[a].length; b++) {
+                    if (data[a][b] == cari){
+                        alamat += "[" +a+ "][" +b+ "}\n" ;
+                        
+                        ditemukan = true;
+                    }
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Ditemukan di index\n" + alamat);
+            nilaibaru = Integer.parseInt(JOptionPane.showInputDialog("Masukkan Nilai Ganti"));
+            for (int a = 0; a < data.length; a++) {
+                for (int b = 0; b < data[a].length; b++) {
+                    if (data[a][b] == cari){
+                        data[a][b] = nilaibaru;
+                    }
+                }
+            }
+            
+            if(!ditemukan){
+                   JOptionPane.showMessageDialog(null, "Data tidak ditemukan ");     
+                    }
+                
+                        //Tampilkan Array setelah perubahan 
+                        System.out.println("\nArray setelah diubah:");
+                        for(int a = 0; a <data.length; a++){
+                            for(int b = 0; b <data[a].length; b++){
+                                System.out.print(data[a][b] + " ");
+                            }
+                            System.out.println();
+                        }
+                        // Jumlah Ganjil
+                        int JumlahGanjil = 0;
+                        
+                        for (int a = 0; a <data.length; a++){
+                        for(int b = 0; b <data[a].length; b++){
+                           
+                            
+                       // cek jika nilainya ganjil (bukan index)
+                            if (b % 2 == 0){
+                            JumlahGanjil += data[a][b];
+                            }
+                        }
+                        }
+                        System.out.println("\nJumlah elemen ganjil = " + JumlahGanjil);
+            }
     }
+
+            
+                 
     
-}
+
+
+    
+    
+
---
+++ src/modulwidhea202557201041/modul2widheaayu202557201041.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modulwidhea202557201041;
+
+/**
+ *
+ * @author asus4
+ */
+public class modul2widheaayu202557201041 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+     String data[][] = {{"Abdul", "\t085646668991", "Kediri"}, {"Kusno", "085646668992", "Trenggalek"}, {"Poniran", "085646668999", "Bojonegoro"}};
+       
+      System.out.println("Nama\t\tAlamat\t\tTelpon");
+       for (int i = 0; i < data.length; i++){
+           System.out.println(data[i][0] + "\t\t" + data[i][2] + "\t" + data[i][1]);
+           
+        }            
+    }
+}
+
+    
+
